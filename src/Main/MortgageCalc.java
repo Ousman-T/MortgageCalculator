44c40318c909ac07ebf3c4f112e4bd9e296ea7de
Ousman Touray <otouray14@gmail.com>
2023-09-05 10:36:19 -0400

    Error handling

--- src/Main/MortgageCalc.java
+++ src/Main/MortgageCalc.java
@@ -6,14 +6,40 @@ import java.util.Scanner;
 public class MortgageCalc {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Principal: ");
-        long principal = scanner.nextLong();
+//        System.out.println("Principal ($1k - $1M): ");
+        long principal;
+        while (true){
+            System.out.println("Principal ($1k - $1M): ");
+            principal = scanner.nextLong();
+            if(principal >= 1000 && principal <= 1000000)
+                break;
+            else{
+                System.out.println("Enter a number between 1,000 and 1,000,000.");
+            }
+        }
 
-        System.out.println("Annual Interest Rate: ");
-        double air = scanner.nextDouble();
+        double air;
+        while (true){
+            System.out.println("Annual Interest Rate (1 - 30): ");
+            air = scanner.nextDouble();
+            if(air > 0 && air < 30)
+                break;
+            else{
+                System.out.println("Enter a value greater than 0 and less than 30.");
+            }
+        }
 
         System.out.println("Loan Period (Years): ");
-        long annualInterest = scanner.nextInt();
+        long annualInterest;
+        while(true){
+            System.out.println("Loan Period (Years 1 - 30): ");
+            annualInterest = scanner.nextLong();
+            if(annualInterest >= 1 && annualInterest <= 30)
+                break;
+            else{
+                System.out.println("Enter a value between 1 and 30.");
+            }
+        }
 
         double monthlyInterest = air / 12;
         double sumInterest = monthlyInterest / 100;
